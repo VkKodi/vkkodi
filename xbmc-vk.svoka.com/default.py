53b50c3810a710773be8b9cbe84ee9c1f4fcdf93
Volodymyr Shcherban <shchvova@gmail.com>
2011-01-09 23:04:16 -0500

    added GPL v3

--- xbmc-vk.svoka.com/default.py
+++ xbmc-vk.svoka.com/default.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Writen by me, yeah! Shchvova (www.svoka.com)
-# Please leave my copyrights here cause as you can notice
-# "Copyright" always means "absolutely right copying".
-# Illegal copying of this code prohibited by real patsan's law!
+# VK-XBMC add-on
+# Copyright (C) 2011 Volodymyr Shcherban
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+__author__ = 'Volodymyr Shcherban'
+
+
 
 import sys, xbmcaddon, xbmc, xbmcgui, xbmcplugin, urllib
 
--- xbmc-vk.svoka.com/xvvideo.py
+++ xbmc-vk.svoka.com/xvvideo.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# This code written by anonymous, but modified by me, Shchvova!
-# Please leave my copyrights here cause as you can notice
-# "Copyright" always means "absolutely right copying".
-# Illegal copying of this code prohibited by real patsan's law!
-__author__ = 'vova'
+# VK-XBMC add-on
+# Copyright (C) 2011 Volodymyr Shcherban
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+__author__ = 'Volodymyr Shcherban'
+
 
 import xbmcgui, xbmc, xbmcplugin, xbmcaddon, datetime, os, urllib, re
 from xml.sax.saxutils import unescape
