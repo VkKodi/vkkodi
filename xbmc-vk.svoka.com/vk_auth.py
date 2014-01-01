7199604e811a547db5b83c656bd77b4330ff512c
Vlad Svoka <shchvova@gmail.com>
2014-01-01 12:29:14 -0500

    small fix for auth

--- xbmc-vk.svoka.com/vk_auth.py
+++ xbmc-vk.svoka.com/vk_auth.py
@@ -10,9 +10,14 @@ except ImportError:
 
 
 def auth(email, password, client_id, secret, scope):
-    url = urllib.urlopen("https://oauth.vk.com/token?grant_type=password&" +
-                         "client_id=%s&client_secret=%s&username=%s&password=%s&scope=%s"
-                         % (client_id, secret, email, password, scope))
+    url = urllib.urlopen("https://oauth.vk.com/token?" + urllib.urlencode({
+            "grant_type": "password",
+            "client_id": client_id,
+            "client_secret": secret,
+            "username": email,
+            "password": password,
+            "scope": scope
+        }))
     out = json.load(url)
     if "access_token" not in out:
         print out
