b29176fb32cbbf8f8555fe6727c4161a7fec301c
Vlad Svoka <shchvova@gmail.com>
2014-01-18 16:03:19 -0500

    removing logout on error

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm -f xbmc-vk.svoka.com.zip
 find xbmc-vk.svoka.com -name "*.pyc" | xargs -I {} rm -v "{}"
+find xbmc-vk.svoka.com -name "*.pyo" | xargs -I {} rm -v "{}"
 find xbmc-vk.svoka.com -name ".DS_Store" | xargs -I {} rm -rfv "{}"
 find xbmc-vk.svoka.com -name ".idea" | xargs -I {} rm -rfv "{}"
 
